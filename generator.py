c62111a6d822d9d923d5c4e86bcea9e14f761a86
MR.X <107016185+cyberytti@users.noreply.github.com>
2022-09-18 20:36:51 +0530

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,10 @@
+import random 
+r = "\033[1;31m"
+w = "\033[0m"
+syntext=["$","()","++","@#$&","£^€÷`¶","§¶","™","}{","‽¿"]
+text=["hello_world","who_am_i","your_dad_is_here","©©©®®®"]
+end=["ggggggafwkoem","utwutd3:e5eid8","iehsusbeuehwjwnwbvsj","shsjdfevdketwkwhsiwtebs","getehejetev99263281936","Good_bye"]
+d=random.choice(syntext)
+b=random.choice(text)
+f=random.choice(end)
+print (r+"PASSWORD:","",w+b+d+f)
